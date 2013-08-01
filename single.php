eee30c72fd14b9492c69396de09dc1b2b8751628
nico <winneras@gmail.com>
2013-08-01 17:02:24 +0100

    add code for comments, but have not complete yet

--- single.php
+++ single.php
@@ -9,15 +9,41 @@
 
             <?php /* Start the Loop */ ?>
             <?php while (have_posts()) : the_post(); ?>
-                <h1>
-                    <?php the_title(); ?>
-                </h1>
-                <div>
-                    <?php the_content(); ?>
-                </div>
+
+
+
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <header class="entry-header">
+                        <h1 class="entry-title">
+                            <a href="<?php the_permalink(); ?>" title="" rel="bookmark"><?php the_title(); ?></a>
+                        </h1>
+                    </header>
+
+                    <div class="entry-content">
+                        <?php the_content(); ?>
+                    </div>
+
+
+                    <footer class="entry-meta">
+                        
+                        <span class="meta_span">Author: <?php the_author() ?></span>
+                        <span class="meta_span">Category: <?php the_category(', ') ?></span>
+                        <span class="meta_span">Post Date: <?php the_time('Y-m-d H:i') ?></span>
+                        <span class="meta_span">Views:<?php if (function_exists(the_views)) { the_views('???', true); } ?></span>
+                        <span class="meta_span">Comments:<?php comments_popup_link('0', '1', '%'); ?></span>
+
+                    </footer>
+                </article>
+
+
+
+
+
             <?php endwhile; ?>
 
             <?php ph_content_nav('nav-below'); ?>
+            
+            <?php comments_template(); ?>
         </div><!-- #content -->
     </div><!-- #primary -->
 </div><!-- #main .wrapper -->
