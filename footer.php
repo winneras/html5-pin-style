0927825a3566cccd5be91ffd6a85fbb680384ba2
WINNERAS <winneras@gmail.com>
2013-08-04 17:53:35 +0100

    footer style

--- footer.php
+++ footer.php
@@ -1,16 +1,18 @@
 
 <footer id="colophon" role="contentinfo">
-    <?php if (is_active_sidebar('widget-footer')) : ?>
-        <div id="footer-wedgits" class="row clearfix">
+    <div class="wrapper">
+        <?php if (is_active_sidebar('widget-footer')) : ?>
+            <div id="footer-wedgits" class="row clearfix">
 
-            <?php dynamic_sidebar('Footer'); ?>
+                <?php dynamic_sidebar('Footer'); ?>
 
-        </div>
-    <?php endif; ?>
-    <div class="site-info">
+            </div>
+        <?php endif; ?>
+        <div class="site-info">
 
 
-    </div><!-- .site-info -->
+        </div><!-- .site-info -->
+    </div>
 </footer><!-- #colophon -->
 </div><!-- #page -->
 
