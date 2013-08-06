148386bd26b75f8720e9fd76bfd6e3ee169fbfa0
nico <winneras@gmail.com>
2013-08-06 14:29:48 +0100

    1. some bug fix 2. search form

---
+++ searchform.php
@@ -0,0 +1,4 @@
+	<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+		<input type="text" class="field" name="s" id="s" placeholder="Looking for ..." />
+                <button type="submit" id="searchsubmit"><span class="icon-search"></span></button>
+	</form>
