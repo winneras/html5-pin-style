3be6573806629acbc5102814c5dcac65512085ba
nico <winneras@gmail.com>
2013-08-06 16:15:11 +0100

    add clearfix class

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-	<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" class="clearfix">
 		<input type="text" class="field" name="s" id="s" placeholder="Looking for ..." />
                 <button type="submit" id="searchsubmit"><span class="icon-search"></span></button>
 	</form>
