1ad02c4052b40450abb91200d01cd3dfa9285f3a
WINNERAS <winneras@gmail.com>
2013-09-30 23:02:59 +0100

    add wrapper inside masonry-item to create margin-left&right add margin-top&bottom in css

--- content-summary.php
+++ content-summary.php
@@ -1,45 +1,44 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(array('col-md-4','col-lg-3','masonry-item')); ?>>
-    <div class="entry-content">
-        <a href="<?php the_permalink(); ?>" title="" rel="bookmark">
-            <?php if (has_post_thumbnail()): ?>
-                <div class="post-thumb">
-                    <?php the_post_thumbnail('category-thumb',array('class' => 'img-responsive')); ?>
-                </div>
-                <div class="post-content nodisplay">
-                <?php else : ?>
-                    <div class="post-content">
-                    <?php endif; ?>
-
+<article id="post-<?php the_ID(); ?>" <?php post_class(array('col-md-4', 'col-lg-3', 'masonry-item')); ?>>
+    <div class="thumbnail">
+        <div class="entry-content">
+            <a href="<?php the_permalink(); ?>" title="" rel="bookmark">
+                <?php if (has_post_thumbnail()): ?>
+                    <div class="post-thumb">
+                        <?php the_post_thumbnail('category-thumb', array('class' => 'img-responsive')); ?>
+                    </div>
+                <?php endif; ?>
+                <div class="post-content <?php if (has_post_thumbnail()): ?>nodisplay<?php endif; ?>">
                     <?php echo mb_strimwidth(strip_tags(apply_filters('the_content', $post->post_content)), 0, 380, "..."); ?>
                 </div>
-        </a>
+            </a>
+        </div>
+
+        <header class="entry-header">
+            <h1 class="entry-title">
+                <a href="<?php the_permalink(); ?>" title="" rel="bookmark"><?php the_title(); ?></a>
+            </h1>
+            <div class="meta-date">
+                <span class="meta_span date">
+                    <span class="month"><?php the_time('M') ?></span>
+                    <span class="date"><?php the_time('d') ?></span>
+                    <span class="year"><?php the_time('Y') ?></span>
+                </span>
+            </div>
+        </header>
+
+        <footer class="entry-meta">
+
+            <div class="meta-info">
+                <span class="meta_span">
+                    <?php if (function_exists(the_views)) : ?>
+                        <span class="icon icon-views"></span>
+                        <?php the_views('???', true); ?>
+                    <?php endif; ?>
+                </span>
+                <span class="meta_span"><span class="icon icon-comments"></span><?php comments_popup_link('0', '1', '%'); ?></span>
+                <span class="meta_span"><span class="icon icon-cats"></span><?php the_category(', ') ?></span>
+                <span class="meta_span"><span class="icon icon-tags"></span><?php the_tags('', ', ', ''); ?></span>
+            </div>
+        </footer>
     </div>
-
-    <header class="entry-header">
-        <h1 class="entry-title">
-            <a href="<?php the_permalink(); ?>" title="" rel="bookmark"><?php the_title(); ?></a>
-        </h1>
-        <div class="meta-date">
-            <span class="meta_span date">
-                <span class="month"><?php the_time('M') ?></span>
-                <span class="date"><?php the_time('d') ?></span>
-                <span class="year"><?php the_time('Y') ?></span>
-            </span>
-        </div>
-    </header>
-
-    <footer class="entry-meta">
-
-        <div class="meta-info">
-            <span class="meta_span">
-                <?php if (function_exists(the_views)) : ?>
-                    <span class="icon icon-views"></span>
-                    <?php the_views('???', true); ?>
-                <?php endif; ?>
-            </span>
-            <span class="meta_span"><span class="icon icon-comments"></span><?php comments_popup_link('0', '1', '%'); ?></span>
-            <span class="meta_span"><span class="icon icon-cats"></span><?php the_category(', ') ?></span>
-            <span class="meta_span"><span class="icon icon-tags"></span><?php the_tags('', ', ', ''); ?></span>
-        </div>
-    </footer>
 </article>
--- libs/js/masonry.custom.js
+++ libs/js/masonry.custom.js
@@ -3,6 +3,6 @@ var container = jQuery('.masonry-container');
 jQuery(window).load(function() {
     container.masonry({
         columnWidth: '.masonry-item',
-        itemSelector: '.masonry-item'
+        itemSelector: '.masonry-item',
     });
 });
